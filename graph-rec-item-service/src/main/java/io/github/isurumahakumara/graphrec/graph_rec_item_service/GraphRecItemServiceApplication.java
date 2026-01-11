62f52b83fcb2dbe12e28753a4d1495c406d8556b
Isuru Mahakumara <imahakumara@gmail.com>
2026-01-11 15:09:53 +0530

    Add REST endpoint for hello world response in GraphRecItemServiceApplication

--- graph-rec-item-service/src/main/java/io/github/isurumahakumara/graphrec/graph_rec_item_service/GraphRecItemServiceApplication.java
+++ graph-rec-item-service/src/main/java/io/github/isurumahakumara/graphrec/graph_rec_item_service/GraphRecItemServiceApplication.java
@@ -2,12 +2,19 @@ package io.github.isurumahakumara.graphrec.graph_rec_item_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GraphRecItemServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraphRecItemServiceApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
 }
