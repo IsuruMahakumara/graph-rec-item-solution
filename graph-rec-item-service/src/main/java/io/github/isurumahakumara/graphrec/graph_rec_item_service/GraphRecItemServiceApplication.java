cd4475113e61cd457d8ebfce5d362c39c31ea803
Isuru Mahakumara <imahakumara@gmail.com>
2026-01-11 15:06:04 +0530

    Initialize graph-rec-item-service with Maven setup, including wrapper scripts, POM file, and basic Spring Boot application structure.

---
+++ graph-rec-item-service/src/main/java/io/github/isurumahakumara/graphrec/graph_rec_item_service/GraphRecItemServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.isurumahakumara.graphrec.graph_rec_item_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphRecItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphRecItemServiceApplication.class, args);
+	}
+
+}
---
+++ graph-rec-item-service/src/test/java/io/github/isurumahakumara/graphrec/graph_rec_item_service/GraphRecItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.isurumahakumara.graphrec.graph_rec_item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphRecItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
